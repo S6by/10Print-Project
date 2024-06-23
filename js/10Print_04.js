21a0dec50b1798c002f5fccbd367521b4a1998e9
Giuppese <giuseppecosta802@gmail.com>
2024-06-23 19:30:59 +0200

    Update 10Print_04.js

--- js/10Print_04.js
+++ js/10Print_04.js
@@ -1,1 +1,36 @@
+let a = 1000;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0)
+}
 
+function draw() {
+  fill(0)
+  cerchio()
+  
+  if(random(1) < 0.5){
+    
+    stroke('white')
+    
+    }else {
+      
+      stroke('black')
+      
+      }
+  
+  
+}
+
+function cerchio(){
+  
+  if (a!=0) {
+  circle(windowWidth/2,windowHeight/2,a)
+  a = a -1;
+ 
+  }else{
+    
+    }
+    
+
+}
+  
