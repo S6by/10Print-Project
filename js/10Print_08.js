1a036ca226a3bd9f5eafe6b9ea3806ce1180417d
Giuppese <giuseppecosta802@gmail.com>
2024-06-23 18:45:02 +0200

    Update 10Print_08.js

--- js/10Print_08.js
+++ js/10Print_08.js
@@ -1,1 +1,21 @@
+let x = 0 
+let y = 0
 
+function setup ( ) {
+  createCanvas ( windowWidth, windowHeight)
+  background (30 )
+  noFill ( )
+}
+
+function draw ( ) {
+  stroke (0, 255, 0)
+  strokeWeight (10)
+  ellipseMode (CORNER)
+  ellipse (x,y,50)
+  
+  x = x + 50
+  if (x > windowWidth ) {
+    x= 0 
+    y = y + 50
+  }
+}
