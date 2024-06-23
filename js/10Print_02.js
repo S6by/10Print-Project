7d1435d502c8ac98fa67fd3a733893ce3e477fb5
Giuppese <giuseppecosta802@gmail.com>
2024-06-23 18:04:55 +0200

    Update 10Print_02.js

--- js/10Print_02.js
+++ js/10Print_02.js
@@ -1,33 +1,29 @@
-let spacing = 80
+let spacing = 60
 let x = 0;
 let y = 0;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(0);
-
+  createCanvas(windowWidth, windowHeight -10);
+  background( 0 );
 }
 
-function draw() {
-  stroke(255);
-  strokeWeight(10);
-  
-  if ( random (1) < 0.5){
-    
-    
-    line( x , y , x , y + spacing );
-  
-  }else{
-  
-   
-    line( x + spacing , y , x , y );
-
+function draw() { 
+  strokeWeight ( 30 )
+  if ( random ( 1 ) < 0.5 ) {
+    //stroke ( 10, 40, 60) 
+     stroke ( random ( 0 ) ,random (255) , random (0) )
+    rect ( x,y,x+spacing,y+spacing );
+    noFill ()
+  } else { 
+  //stroke ( 30, 70, 80) 
+    stroke ( random ( 0 ) ,random (300), random (0) )
+    rect ( y, x+spacing,x,y+spacing );
+    noFill ()
   }
+  x += spacing;
+  if ( x > windowWidth) {
+    x=0
+    y +=spacing
   
-    x = x + spacing;
-  
-  if (x > windowWidth){
-    x = 0;
-    y = y +spacing;
   }
-
+    
 }
