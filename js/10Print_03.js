ea928abf610c4e68a510fb0ea33a7b9df87b8b10
Giuppese <giuseppecosta802@gmail.com>
2024-06-23 18:12:39 +0200

    Update 10Print_03.js

--- js/10Print_03.js
+++ js/10Print_03.js
@@ -1,34 +1,37 @@
-let spacing = 80
-let x = 0;
-let y = 0;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(0);
+let x=0
+let y=0
+let spacing=20
 
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(255);
+  //stroke(random(255),random(255),random(255))
+  
+}
+
+function draw(){
+  //triangle(x1, y1, x2, y2, x3, y3)
 }
 
 function draw() {
-  stroke(255);
   strokeWeight(10);
-  
-  if ( random (1) < 0.5){
+  //stroke(100,245,20);
+  if (random(1) < 0.5) {
     
-    
-    line( x , y , x , y + spacing );
-  
-  }else{
-  
-   
-    line( x + spacing , y , x , y );
-
+    //stroke(random(255),random(255),random(255))
+    stroke(0)
+    //line(0,20,40,20);
+    line(x,y+spacing,x+spacing*2,y+spacing)
+  } else {
+    //stroke(random(255),random(255),random(255))
+    stroke(0)
+    //line(20,0,20,40);
+    line(x+spacing,y,x+spacing,y+spacing*2)
   }
-  
-    x = x + spacing;
-  
-  if (x > windowWidth){
-    x = 0;
-    y = y +spacing;
+  x=x+spacing
+  if (x> width){
+    x=0;
+    y=y+spacing;
   }
-
-}
-
+} 
